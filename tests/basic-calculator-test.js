e80abb09158b3fad2e6317c2e2af940b12fddf89
aloremia-freedom <aemielvin.loremia@freedom.tm>
2017-08-16 11:14:01 +0800

    Initial calculator implementation (#3)
    * Add more tests
    
    * Implement Calculator

--- tests/basic-calculator-test.js
+++ tests/basic-calculator-test.js
@@ -12,4 +12,28 @@ describe('Calculator', () => {
 	it('Calculator should not be null', () => {
 		expect(calc !== null).to.equal(true);
 	});
+	it('add() should return 10 when passed with 7 and 3', () => {
+		expect(calc.add(7, 3)).to.equal(10);
+	});
+	it('add() should return -2 when passed with -5 and 3', () => {
+		expect(calc.add(-5, 3)).to.equal(-2);
+	});
+	it('subtact() should return 10 when passed with 15 and 5', () => {
+		expect(calc.subtract(15, 5)).to.equal(10);
+	});
+	it('subtact() should return 15 when passed with 10 and -5', () => {
+		expect(calc.subtract(10, -5)).to.equal(15);
+	});
+	it('subtact() should return -15 when passed with -10 and 5', () => {
+		expect(calc.subtract(-10, 5)).to.equal(-15);
+	});
+	it('multiply() should return 6 when passed with 3 and 2', () => {
+		expect(calc.multiply(3, 2)).to.equal(6);
+	});
+	it('multiply() should return -6 when passed with 3 and -2', () => {
+		expect(calc.multiply(3, -2)).to.equal(-6);
+	});
+	it('multiply() should return 6 when passed with -3 and -2', () => {
+		expect(calc.multiply(-3, -2)).to.equal(6);
+	});
 } );

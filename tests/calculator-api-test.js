14f113fcf997dcb099d7f8e445929e504596d7cf
aloremia-freedom <aemielvin.loremia@freedom.tm>
2017-08-16 13:01:56 +0800

    Add tests for API functions

--- tests/calculator-api-test.js
+++ tests/calculator-api-test.js
@@ -1,12 +1,45 @@
 let chai = require('chai');
 let expect = chai.expect;
 let supertest = require('supertest');
-let api = supertest('httpL//localhost:3030');
+let api = supertest('http://localhost:3030');
 
 describe('CalculatorAPI', (done) => {
 	it('should return a 200 response', (done) => {
-		api .get('/presence/')
+		api .get('/')
 			.set('Accept', 'application/json')
 			.expect(200, done);
+	});
+	it('should return 2 + 3 = 5', (done) => {
+		api .get('/add/2/3')
+			.set('Accept', 'application/json')
+			.expect(200)
+			.end((err, res) => {
+				expect(res.body.a).to.equal(2); 
+				expect(res.body.b).to.equal(3); 
+				expect(res.body.result).to.equal(5);
+				done();
+			});
+	});
+	it('should return 2 - 3 = -1', (done) => {
+		api .get('/subtract/2/3')
+			.set('Accept', 'application/json')
+			.expect(200)
+			.end((err, res) => {
+				expect(res.body.a).to.equal(2); 
+				expect(res.body.b).to.equal(3); 
+				expect(res.body.result).to.equal(-1); 
+				done();
+			});
+	});
+	it('should return 2 * 3 = 6', (done) => {
+		api .get('/multiply/2/3')
+			.set('Accept', 'application/json')
+			.expect(200)
+			.end((err, res) => {
+				expect(res.body.a).to.equal(2); 
+				expect(res.body.b).to.equal(3); 
+				expect(res.body.result).to.equal(6); 
+				done();
+			});
 	});
 });
